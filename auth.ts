98eb5871afed93618dc451caf01b752e9a6d6d8b
olarijulius-wq <olarijulius@gmail.com>
2026-02-15 12:25:35 +0200

    Security: disable dangerous OAuth email account linking

--- auth.ts
+++ auth.ts
@@ -349,7 +349,6 @@ const oauthProviders = [];
 if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   oauthProviders.push(
     Google({
-      allowDangerousEmailAccountLinking: true,
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
@@ -366,7 +365,6 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
 if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
   oauthProviders.push(
     GitHub({
-      allowDangerousEmailAccountLinking: true,
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
